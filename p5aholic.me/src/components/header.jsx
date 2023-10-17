54dcc1b8b9707a7848fa9ed254dae2c75fc663a6
Sofía Fornaroli <Sofia0101>
2023-10-17 20:30:51 -0300

    cambio a pagina a app react

---
+++ p5aholic.me/src/components/header.jsx
@@ -0,0 +1,36 @@
+function Header () {
+    return (
+        <header class="siteHeader" id="SiteHeader">
+        <h1 class="siteHeader_title">Keita Yamada</h1>
+        <p class="siteHeader_label">Designer & Developer</p>
+        <nav class="siteHeader_nav" x-data>
+          <ol>
+            <li class="{ 'is-selected': $store.app.currentPath === '/' }">
+              <div class="_dot">&#x25cf;</div>
+              <a class="_text" href="index.html">Home</a>
+            </li>
+            <li
+              class="{ 'is-selected': $store.app.currentPath === '/projects/' }"
+            >
+              <div class="_dot">&#x25cf;</div>
+              <a class="_text" href="projects/index.html">Projects</a>
+            </li>
+            <li 
+            class="{ 'is-selected:' $store.app.currentPath === '/info/' }"
+            >
+              <div class="_dot">&#x25cf;</div>
+              <a class="_text" href="info/index.html">Info</a>
+            </li>
+            <li
+              class="{ 'is-selected': $store.app.currentPath === '/contact/' }"
+            >
+              <div class="_dot">&#x25cf;</div>
+              <a class="_text" href="contact/index.html">Contact</a>
+            </li>
+          </ol>
+        </nav>
+      </header>
+    )
+};
+
+export default Header;
