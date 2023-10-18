2f8429fe57f3ba3524f4c437a0adf66f2c06ca74
Sofía Fornaroli <Sofia0101>
2023-10-18 01:37:56 -0300

    modifico app.js

--- p5aholic.me/src/App.js
+++ p5aholic.me/src/App.js
@@ -2,17 +2,28 @@ import "./App.css";
 import InfoHeader from "./components/info-header";
 import Header from "./components/header";
 import Aboutme from "./components/aboutme";
+import Proyects from "./components/proyects";
+import Info from "./components/info";
+import Contacto from "./components/contact";
+
 
 function App() {
   return (
+    <> 
     <div>
+    
       <Header />
 
       <InfoHeader />
       <main class="content" id="Content" data-scroll="area"></main>
       <div class="content_inner" data-scroll="target"></div>
       <Aboutme />
+      <Proyects />
+      <Info />
+      <Contacto />
+
     </div>
+    </>
   );
 }
 
