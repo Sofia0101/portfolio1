7fc92a05e4eab6a920c91b88b2b7da04b505078a
Sofía Fornaroli <Sofia0101>
2023-10-18 01:42:27 -0300

    Agrego componente contacto

---
+++ p5aholic.me/src/components/contact.jsx
@@ -0,0 +1,28 @@
+function Contacto() {
+  return (
+    <section class="page" data-page="contact">
+      <div class="page_content contact">
+        <div class="contact_wrap">
+          <a
+            class="contact_text jp text-btn"
+            href="../p5aholic.notion.site/p5aholic/Web-aac658c501b8458080dcc19e5ff26e51.html"
+            target="_blank"
+            rel="noopener">
+            Contact Form ↗
+          </a>
+          <a
+            class="contact_text en text-btn"
+            href="../p5aholic.notion.site/p5aholic/Contact-f7a0724e15d44eaebddbf7f09459aef4.html"
+            target="_blank"
+            rel="noopener">
+
+            Contact Form 2 ↗
+          </a>
+        </div>
+      </div>
+    </section>
+
+  );
+}
+
+export default Contacto;
