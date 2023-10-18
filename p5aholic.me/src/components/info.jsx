bc98be066acf15bd8aefe8b77a4b7cb8c5510c8c
Sofía Fornaroli <Sofia0101>
2023-10-18 01:42:49 -0300

    Agrego componente info

---
+++ p5aholic.me/src/components/info.jsx
@@ -0,0 +1,51 @@
+function Info () {
+    return (
+        <section class="page" data-page="info">
+        <div class="info_item info_item-2">
+          <div class="info_content">
+            <ul>
+              <li>
+                <a
+                  class="text-btn"
+                  href="../experiments.p5aholic.me/index.html"
+                  target="_blank"
+                  rel="noopener"
+                  >experiments.p5aholic.me ↗</a>
+              </li>
+              <li>
+                <a
+                  class="text-btn"
+                  href="../playground.p5aholic.me/index.html"
+                  target="_blank"
+                  rel="noopener"
+                  >playground.p5aholic.me ↗</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="info_item info_item-3">
+          <div class="info_content">
+            <ul>
+              <li>
+                <span class="is-light"
+                  >Awwwards Independent of the Year<br /></span>
+                   <span>2020 Nominee</span>
+              </li>
+              <li>
+                <span class="is-light"
+                  >Awwwards Site of the Day<br /></span>
+                  <span>Web Graphic Experiments</span>
+              </li>
+              <li>
+                <span class="is-light"
+                  >Awwwards Site of the Day<br /></span>
+                  <span>100 DAYS OF POETRY</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </section>
+    );
+}
+
+export default Info;
