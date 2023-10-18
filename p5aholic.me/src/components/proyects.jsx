765a3eb381bb22b35945ec7dc0399bdef22a1a55
Sofía Fornaroli <Sofia0101>
2023-10-18 01:43:17 -0300

    Agrego componente proyects

---
+++ p5aholic.me/src/components/proyects.jsx
@@ -0,0 +1,153 @@
+function Proyects() {
+  return (
+    <section class="page" data-page="projects">
+      <div class="page_content project">
+        <div class="project_section">
+          <h2 class="project_heading">&#x25a0; Projects (Design & Develop)</h2>
+          <div class="project_list">
+            <a
+              class="project_item text-btn"
+              href="../midnight-grand-orchestra.jp/starpeggio/index.html"
+              target="_blank">
+              <div class="project_title">Starpeggio</div>
+              <div class="project_info">2023 / Commission / Special</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../bnn.co.jp/index.html"
+              target="_blank">
+              <div class="project_title">BNN Online Store</div>
+              <div class="project_info">2023 / Commission / Corporate</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../ily-girl.mikapikazo.info/index.html"
+              target="_blank">
+              <div class="project_title">ILY GIRL</div>
+              <div class="project_info">2023 / Commission / Special</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../midnight-grand-orchestra.jp/overture-bluray-dvd/index.html"
+              target="_blank" >
+              <div class="project_title">OVERTURE Blu-Ray/DVD</div>
+              <div class="project_info">2023 / Commission / Special</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../yansukim.com/index.html"
+              target="_blank">
+              <div class="project_title">yansuKIM</div>
+              <div class="project_info">2023 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../punchred.xyz/index.html"
+              target="_blank">
+              <div class="project_title">PUNCH</div>
+              <div class="project_info">2022 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../solani-illustration.xyz/index.html"
+              target="_blank">
+              <div class="project_title">SOLANI</div>
+              <div class="project_info">2022 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../revenge-pop.mikapikazo.info/index.html"
+              target="_blank">
+              <div class="project_title">REVENGE POP</div>
+              <div class="project_info">2022 / Commission / Special</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../holo.punchred.xyz/index.html"
+              target="_blank">
+              <div class="project_title">HOLO.g</div>
+              <div class="project_info">2021 / Commission / Special</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../pan-projects.com/index.html"
+              target="_blank">
+              <div class="project_title">PAN- PROJECTS</div>
+              <div class="project_info">2021 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../yoshikiokamura.art/index.html"
+              target="_blank">
+              <div class="project_title">Yoshiki Okamura</div>
+              <div class="project_info">2021 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../buoy.work/index.html"
+              target="_blank">
+              <div class="project_title">buoy</div>
+              <div class="project_info">2020 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../experiments.p5aholic.me/index.html"
+              target="_blank">
+              <div class="project_title">Experiments</div>
+              <div class="project_info">2020 / Personal / Gallery</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../100daysofpoetry.gallery/index.html"
+              target="_blank" >
+              <div class="project_title">100 DAYS OF POETRY</div>
+              <div class="project_info">2020 / Commission / Gallery</div>
+            </a>
+          </div>
+        </div>
+        <div class="project_section">
+          <h2 class="project_heading">&#x25a0; Projects (Only Develop)</h2>
+          <div class="project_list">
+            <a
+              class="project_item text-btn"
+              href="../vortex-live.com/index.html"
+              target="_blank">
+              <div class="project_title">vortex</div>
+              <div class="project_info">2023 / Commission / Corporate</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../reinakanishi.com/index.html"
+              target="_blank">
+              <div class="project_title">REI NAKANISHI</div>
+              <div class="project_info">2022 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../vixi-vixi.jp/index.html"
+              target="_blank">
+              <div class="project_title">VIXI</div>
+              <div class="project_info">2021 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../signif.jp/index.html"
+              target="_blank">
+              <div class="project_title">Signif</div>
+              <div class="project_info">2020 / Commission / Portfolio</div>
+            </a>
+            <a
+              class="project_item text-btn"
+              href="../nanameinc.jp/index.html"
+              target="_blank">
+              <div class="project_title">naname</div>
+              <div class="project_info">2020 / Commission / Portfolio</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Proyects;
