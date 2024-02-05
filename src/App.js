f635a0f7bc7c59454f64e7ba239a80b35f2ce8ce
Auca Maillot <aucacoyan@gmail.com>
2024-02-05 16:57:05 -0300

    elimine carousel

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./css/background.css";
 import "./css/contact.css";
 import "./css/icons.css";
 import "./css/proyects.css";
-import "./css/carousel.css"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
@@ -14,7 +13,6 @@ import Aboutme from "./components/aboutme";
 import Proyects from "./components/proyects";
 import Info from "./components/icons";
 import Navi from "./components/NavBar";
-import UncontrolledExample from "./components/slides";
 
 function App() {
   return (
@@ -25,9 +23,7 @@ function App() {
           <Header />
           <main className="content" id="Content" data-scroll="area"></main>
           <div className="content_inner" data-scroll="target"></div>
-          <Aboutme />
-          <UncontrolledExample />
-          <Proyects />
+          <Aboutme />          <Proyects />
           <Info />
         </div>
       </section>
