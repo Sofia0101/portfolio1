bb2f0907ecae29acce159713c6c2dfe4f24ca69e
Sofía Fornaroli <Sofia0101>
2024-02-12 16:18:11 -0300

    agrego animación Fade

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,12 +1,12 @@
-
+import { Fade } from "react-awesome-reveal";
 function Description() {
     return (
       <section className="description">
         <div className="description_div">
-          <p className="description_p">
+        <Fade direction="down">  <p className="description_p">
           A long time ago I entered the world of programming, and since then I haven't stopped learning, working and having fun with it! <br /> These are some of the languages ​​I learned and applied on this journey
           :)
-          </p>
+          </p></Fade>
           {/* <img className="arg__imagen" src={argentina} alt="Argentina" /> */}
         </div>
 
