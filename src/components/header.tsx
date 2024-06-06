2a68422db2ddc9906b68692d8d31acbcd3f1e516
Sofía Fornaroli <Sofia0101>
2024-06-06 14:20:06 -0300

    limpio el codigo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@ function Header () {
         <header className="siteHeader" id="SiteHeader">
       <div className="Header__t"> 
        <h1 className="siteHeader_title1">Hi!</h1> <h1 className="siteHeader_title"> I'm  Sofía Fornaroli.</h1>
-        {/* <p className="siteHeader_label">I'm a Frontend Developer</p>   */}
         </div>
 
         </header>
